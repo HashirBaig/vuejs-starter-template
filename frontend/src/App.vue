79e3f9aafe357cf4ad68f65148f8fc2aae31423d
HashirBaig <hashirbaig456@gmail.com>
2023-06-23 15:51:34 +0500

    Added check for multi tab logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,20 @@
 <script setup>
+
+import { onMounted } from 'vue';
+import { useStore } from 'vuex';
+
+
+const store = useStore()
+
+onMounted(() => {
+  // Logout users from all tabs if they logout from one tab.
+  window.addEventListener("storage", () => {
+    if (!localStorage.token && !localStorage.ga && !localStorage.id) {
+      store.dispatch("logout")
+    }
+  })
+})
+
 </script>
 
 <template>
